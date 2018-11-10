5b6b738487722320a0805b8240d1019b3483d9dc
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-11-10 19:29:23 +0100

    move KleisliBracketInstance to new data-instances-effects module

--- modules/effects/arrow-data-instances-effects/src/main/kotlin/arrow/instances/kleisli.kt
+++ modules/effects/arrow-data-instances-effects/src/main/kotlin/arrow/instances/kleisli.kt
@@ -8,29 +8,26 @@ import arrow.effects.typeclasses.Bracket
 import arrow.effects.typeclasses.ExitCase
 import arrow.extension
 import arrow.instances.KleisliMonadErrorInstance
-import arrow.typeclasses.MonadError
 
 @extension
 interface KleisliBracketInstance<F, R, E> : Bracket<KleisliPartialOf<F, R>, E>, KleisliMonadErrorInstance<F, R, E> {
 
-  fun BF(): Bracket<F, E>
-
-  override fun ME(): MonadError<F, E> = BF()
+  override fun FFF(): Bracket<F, E>
 
   override fun <A, B> Kind<KleisliPartialOf<F, R>, A>.bracketCase(
-    release: (A, ExitCase<E>) -> Kind<KleisliPartialOf<F, R>, Unit>,
-    use: (A) -> Kind<KleisliPartialOf<F, R>, B>
+    use: (A) -> Kind<KleisliPartialOf<F, R>, B>,
+    release: (A, ExitCase<E>) -> Kind<KleisliPartialOf<F, R>, Unit>
   ): Kleisli<F, R, B> =
-    BF().run {
+    FFF().run {
       Kleisli { r ->
-        this@bracketCase.fix().run(r).bracketCase({ a, br ->
-          release(a, br).fix().run(r)
-        }) { a ->
+        this@bracketCase.fix().run(r).bracketCase({ a ->
           use(a).fix().run(r)
-        }
+        }, { a, br ->
+          release(a, br).fix().run(r)
+        })
       }
     }
 
   override fun <A> Kind<KleisliPartialOf<F, R>, A>.uncancelable(): Kleisli<F, R, A> =
-    Kleisli { r -> BF().run { this@uncancelable.fix().run(r).uncancelable() } }
+    Kleisli { r -> FFF().run { this@uncancelable.fix().run(r).uncancelable() } }
 }
